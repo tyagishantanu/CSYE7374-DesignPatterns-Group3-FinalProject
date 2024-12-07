4f99d834e1cfc47b4c1b9ec051d1027fa1415d93
jazibjavaid <javaid.j@northeastern.edu>
2024-12-07 13:11:06 -0500

    Updated vehicle class and fixed bugs in vehiclebuilder

--- VehicleDealershipInventoryManagement/src/main/java/edu/neu/csye7374/Builder/VehicleBuilder.java
+++ VehicleDealershipInventoryManagement/src/main/java/edu/neu/csye7374/Builder/VehicleBuilder.java
@@ -1,79 +1,79 @@
 package edu.neu.csye7374.Builder;
 
-import edu.neu.csye7374.Car;
-import edu.neu.csye7374.CarCategory;
+import edu.neu.csye7374.Vehicle;
+import edu.neu.csye7374.VehicleCategory;
 import edu.neu.csye7374.Factory.*;
 
 public class VehicleBuilder implements BuilderAPI {
 
-    private int carId;
-    private String carName;
-    private double carPrice;
-    private CarCategory carCategory;
-    private String carManufacturer;
+    private int vehicleId;
+    private String vehicleName;
+    private double vehiclePrice;
+    private VehicleCategory vehicleCategory;
+    private String vehicleManufacturer;
 
-    public String getCarManufacturer() {
-        return carManufacturer;
+    public String getVehicleManufacturer() {
+        return vehicleManufacturer;
     }
 
-    public void setCarManufacturer(String carManufacturer) {
-        this.carManufacturer = carManufacturer;
+    public void setVehicleManufacturer(String vehicleManufacturer) {
+        this.vehicleManufacturer = vehicleManufacturer;
     }
 
-    public int getCarId() {
-        return carId;
+    public int getVehicleId() {
+        return vehicleId;
     }
 
-    public void setCarId(int carId) {
-        this.carId = carId;
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
     }
 
-    public String getCarName() {
-        return carName;
+    public String getVehicleName() {
+        return vehicleName;
     }
 
-    public void setCarName(String carName) {
-        this.carName = carName;
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
     }
 
-    public double getCarPrice() {
-        return carPrice;
+    public double getVehiclePrice() {
+        return vehiclePrice;
     }
 
-    public void setCarPrice(double carPrice) {
-        this.carPrice = carPrice;
+    public void setVehiclePrice(double vehiclePrice) {
+        this.vehiclePrice = vehiclePrice;
     }
 
-    public CarCategory getCarCategory() {
-        return carCategory;
+    public VehicleCategory getVehicleCategory() {
+        return vehicleCategory;
     }
 
-    public void setCarCategory(CarCategory carCategory) {
-        this.carCategory = carCategory;
+    public void setVehicleCategory(VehicleCategory vehicleCategory) {
+        this.vehicleCategory = vehicleCategory;
     }
 
-    public VehicleBuilder(int carId, String carName, double carPrice, CarCategory carCategory,
-                          String carManufacturer) {
+    public VehicleBuilder(int vehicleId, String vehicleName, double vehiclePrice, VehicleCategory vehicleCategory,
+                          String vehicleManufacturer) {
         super();
-        this.carId = carId;
-        this.carName = carName;
-        this.carPrice = carPrice;
-        this.carCategory = carCategory;
-        this.carManufacturer = carManufacturer;
+        this.vehicleId = vehicleId;
+        this.vehicleName = vehicleName;
+        this.vehiclePrice = vehiclePrice;
+        this.vehicleCategory = vehicleCategory;
+        this.vehicleManufacturer = vehicleManufacturer;
     }
 
     public VehicleBuilder(String s) {
         String[] tokens = s.split(",");
-        this.carId = Integer.parseInt(tokens[0]);
-        this.carName = tokens[1];
-        this.carPrice = Double.parseDouble(tokens[4]);
-        this.carCategory = CarCategory.getCarCategory(tokens[2]);
-        this.carManufacturer = tokens[3];
+        this.vehicleId = Integer.parseInt(tokens[0]);
+        this.vehicleName = tokens[1];
+        this.vehiclePrice = Double.parseDouble(tokens[4]);
+        this.vehicleCategory = VehicleCategory.getVehicleCategory(tokens[2]);
+        this.vehicleManufacturer = tokens[3];
     }
 
     @Override
-    public Car build() {
+    public Vehicle build() {
         // TODO Auto-generated method stub
-        return CarFactory.getInstance().getObject(this);
+        return VehicleFactory.getInstance().getObject(this);
     }
 }
--- VehicleDealershipInventoryManagement/src/main/java/edu/neu/csye7374/Vehicle.java
+++ VehicleDealershipInventoryManagement/src/main/java/edu/neu/csye7374/Vehicle.java
@@ -1,4 +1,97 @@
 package edu.neu.csye7374;
 
-public class Vehicle {
+import edu.neu.csye7374.Builder.VehicleBuilder;
+
+public class Vehicle implements VehicleAPI {
+    public int vehicleId;
+    public String vehicleName;
+    public double vehiclePrice;
+    public VehicleCategory vehicleCategory;
+    public Object vehicleManufacturer;
+
+    public Vehicle(VehicleBuilder builder) {
+        super();
+        this.vehicleId = builder.getVehicleId();
+        this.vehicleName = builder.getVehicleName();
+        this.vehiclePrice = builder.getVehiclePrice();
+        this.vehicleCategory = builder.getVehicleCategory();
+        this.vehicleManufacturer = builder.getVehicleManufacturer();
+    }
+
+    public Object getVehicleManufacturer() {
+        return String.class.cast(vehicleManufacturer);
+    }
+
+    @Override
+    public void setVehicleManufacturer(String VehicleManufacturer) {
+        this.vehicleManufacturer = VehicleManufacturer;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public double getVehiclePrice() {
+        return vehiclePrice;
+    }
+
+    public void setVehiclePrice(double VehiclekPrice) {
+        this.vehiclePrice = vehiclePrice;
+    }
+
+    public VehicleCategory getVehicleCategory() {
+        return vehicleCategory;
+    }
+
+    public void setVehicleCategory(VehicleCategory vehicleCategory) {
+        this.vehicleCategory = vehicleCategory;
+    }
+
+    @Override
+    public String vehicleDescription() {
+        // TODO Auto-generated method stub
+        return this.vehicleName + ", category : " + this.vehicleCategory;
+    }
+
+    @Override
+    public int noOfVehiclesRented() {
+        // TODO Auto-generated method stub
+        return (int) Math.random() * 5;
+    }
+
+    public double getVehicleRentPrice(){
+        double rentPrice = this.vehiclePrice * 0.05;
+        rentPrice = Math.round(rentPrice * 100.0) / 100.0;
+        return rentPrice;
+    }
+
+    @Override
+    public String toString() {
+        return vehicleId + "," + vehicleName + "," + vehicleCategory + "," + vehicleManufacturer + "," + vehiclePrice;
+    }
+
+
+
+    public String buyVehicle() {
+        return this.toString() + " is bought!!!!";
+    }
+
+    public String rentVehicle() {
+        double rentPrice = this.vehiclePrice * 0.05;
+        rentPrice = Math.round(rentPrice * 100.0) / 100.0;
+        this.vehiclePrice = rentPrice;
+        return this.toString() + " is rented!!";
+    }
 }
